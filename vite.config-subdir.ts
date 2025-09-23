5cd4b76d0becb5b31c998ecd0ef00f1ff9a25b2c
Andreas Kugel <info@digital-codes.de>
2025-09-24 01:09:32 +0200

    build for subdir

--- vite.config-subdir.ts
+++ vite.config-subdir.ts
@@ -4,6 +4,7 @@ import { fileURLToPath, URL } from 'node:url';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/wsswhow/', // Set your subdirectory here
   plugins: [vue()],
   resolve: {
     alias: {
